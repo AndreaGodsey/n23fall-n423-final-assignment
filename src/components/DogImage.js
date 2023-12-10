d4fdce4501c9cf60ce1330bd1adc47d480f6af1c
AndreaGodsey <noirmenace101@gmail.com>
2023-12-10 01:16:56 -0500

    Unnecessary Code
    This was a test

--- src/components/DogImage.js
+++ src/components/DogImage.js
@@ -1,15 +1,11 @@
 import Link from 'next/link';
 import React from 'react';
 import {Popup, Button, Image, Grid} from 'semantic-ui-react';
-import useAppState from '@/useHooks/useAppState';
+
 
 export default function DogImage({src, children, onClick, id}) {
-    const appState = useAppState();
-    function deleteDog(selectedDog) {
-        appState.updateAppState({
-            Dogs: appState.Dogs.concat(selectedDog, 3),
-        });
-    }
+
+   
     return (
         <>
         
@@ -20,7 +16,6 @@ export default function DogImage({src, children, onClick, id}) {
     content={(<>
     <Button color='orange' icon='heart' content={children || 'Save'} onClick={onClick} />
     <Button color='blue' icon='search' content={'More Details'} as={Link} href={`/dogs/${id}`}/>
-    <Button color='red' icon='trash' content={'Delete Image'} onClick={deleteDog}/>
     </>
     )}
     />
